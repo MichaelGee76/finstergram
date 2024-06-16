ebaef1a55e664f48c5971ad687cba74508bc33d6
Zied1981 <zkhemiri@gmail.com>
2024-06-16 11:28:03 +0200

    sunday push

--- backend/src/models/post.js
+++ backend/src/models/post.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 const postSchema = new mongoose.Schema(
-    {
-        userId: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+  {
+    userId: { type: mongoose.Types.ObjectId, ref: "User", required: true },
 
-        picture: { type: String, required: true },
-        description: { type: String },
-        hashtags: [{ type: String }],
-    },
-    { collection: "post", timestamps: true }
+    picture: { type: String, required: true },
+    description: { type: String },
+    hashtags: [{ type: String }],
+  },
+  { collection: "post", timestamps: true }
 );
 export const Post = mongoose.model("Post", postSchema);
