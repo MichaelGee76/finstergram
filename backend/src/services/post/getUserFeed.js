9e51be3c9bd8213143f43bc7995f7826125b9537
MichaelGee76 <gottungmichael@gmail.com>
2024-06-17 13:06:25 +0200

    feed mit likes?

--- backend/src/services/post/getUserFeed.js
+++ backend/src/services/post/getUserFeed.js
@@ -32,13 +32,36 @@ export async function getUserFeed(authenticatedUserId) {
     if (!feed) {
         throw new Error("Oops, something went wrong. We could not load feed.");
     }
-    const postIds = feed.map((post) => post.toObject()._id);
+    // Freddys Ansatz
 
+    // const postIds = feed.map((post) => post.toObject()._id);
+
+    // const likes = await Like.find({
+    //     postId: { $in: postIds },
+    // }).map((like) => like.postId.toString());
+
+    // Der Ansatz vom Inder
+    // Die IDs der Posts extrahieren
+    const postIds = feed.map((post) => post._id);
+
+    // Likes für die Posts abrufen und die Anzahl der Likes zählen
     const likes = await Like.find({
         postId: { $in: postIds },
-    }).map((like) => like.postId.toString());
+    });
 
-    return feed;
+    // Ein Objekt erstellen, das die Anzahl der Likes für jeden Post speichert
+    const likeCounts = {};
+    likes.forEach((like) => {
+        likeCounts[like.postId] = (likeCounts[like.postId] || 0) + 1;
+    });
+
+    // Die Anzahl der Likes zu den Posts hinzufügen
+    const feedWithLikes = feed.map((post) => {
+        const postObject = post.toObject();
+        postObject.likes = likeCounts[postObject._id] || 0;
+        return postObject;
+    });
+    return feedWithLikes;
 }
 
 // import { Follow } from "../../models/follow.js";
