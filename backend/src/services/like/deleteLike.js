ff29848af2f60416d31fe67a4c1b96c9d3e86f05
MichaelGee76 <gottungmichael@gmail.com>
2024-06-17 18:42:38 +0200

    likedelete fixed

--- backend/src/services/like/deleteLike.js
+++ backend/src/services/like/deleteLike.js
@@ -5,12 +5,22 @@ export async function deleteLike(userId, postId, commentId) {
     if (!existingLike) {
         throw new Error("Cannot like");
     }
-    // abfrage ob , commentId wenn ja dann 3 parameter übergeben
+
+    if (commentId) {
+        const deletedComment = await Like.findOneAndDelete({
+            userId,
+            postId,
+            userId,
+        });
+        if (!deletedComment) throw new Error("Could not delete comment");
+        return { message: "CommentLike deleted" };
+    }
+
     const deletedLike = await Like.findOneAndDelete({ userId, postId });
 
     if (!deletedLike) {
         throw new Error("Cannot delete like  ");
     }
 
-    return { message: "Like deleted" };
+    return { message: "PostLike deleted" };
 }
