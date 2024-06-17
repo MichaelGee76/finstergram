fff9d54fe5d13d1c6c7ce1ae59fbf1d9873360b9
MichaelGee76 <gottungmichael@gmail.com>
2024-06-17 12:13:53 +0200

    userFeed update

--- backend/src/routes/postRouter.js
+++ backend/src/routes/postRouter.js
@@ -3,14 +3,14 @@ import { PostController } from "../controllers/postController.js";
 import { doJWTAuth } from "../middlewares/doJwtAuth.js";
 
 export const postRouter = express
-  .Router()
-  .get("/userPosts/:id", doJWTAuth, PostController.getUserPostsCtrl)
-  .get("/userFeed", doJWTAuth, PostController.getUserFeedCtrl)
-  .post("/newPost", doJWTAuth, PostController.postNewPostCtrl)
-  .patch("/updatePost/:postId", doJWTAuth, PostController.updatePostCtrl)
-  .delete("/deletePost/:postId", doJWTAuth, PostController.deletePostCtrl)
-  .get(
-    "/allPostsWithHashtag",
-    // doJWTAuth,
-    PostController.getAllPostsWithHashtagsCtrl
-  );
+    .Router()
+    .get("/userPosts/:id", doJWTAuth, PostController.getUserPostsCtrl)
+    .get("/userFeed", doJWTAuth, PostController.getUserFeedCtrl)
+    .post("/newPost", doJWTAuth, PostController.postNewPostCtrl)
+    .patch("/:postId", doJWTAuth, PostController.updatePostCtrl)
+    .delete("/:postId", doJWTAuth, PostController.deletePostCtrl)
+    .get(
+        "/allPostsWithHashtag",
+        // doJWTAuth,
+        PostController.getAllPostsWithHashtagsCtrl
+    );
