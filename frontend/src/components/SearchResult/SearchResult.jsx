087194174886c78322ed1cec66c84c532695e4c1
Frederick Pawelzik <frederickpawelzik@gmail.com>
2024-06-18 14:31:06 +0200

    search site start

---
+++ frontend/src/components/SearchResult/SearchResult.jsx
@@ -0,0 +1,7 @@
+import "./SearchResult.css";
+
+const SearchResult = () => {
+  return <article></article>;
+};
+
+export default SearchResult;
--- frontend/src/pages/Search/Search.jsx
+++ frontend/src/pages/Search/Search.jsx
@@ -1,6 +1,61 @@
+import { useEffect, useState } from "react";
 import "./Search.css";
+import ky from "ky";
+import { backendUrl } from "../../api/api";
 const Search = () => {
-  return <section></section>;
+  const [searchToggle, setSearchToggle] = useState(true);
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    const searchHandler = async () => {
+      if (searchToggle) {
+        const res = await ky.get(`${backendUrl}/users`).json();
+
+        setSearchResults(res.result);
+      }
+    };
+    searchHandler();
+  }, []);
+
+  return (
+    <main className="search_site">
+      <form>
+        <label htmlFor="">
+          <img src="./img/SearchUnclicked.svg" alt="" />
+        </label>
+        <input
+          type="text"
+          placeholder={searchToggle ? "username.." : "hashtag.."}
+        />
+        {/* <button></button> */}
+      </form>
+
+      <div className="search_btns_wrapper">
+        <button onClick={() => setSearchToggle(true)}>
+          <img
+            src={
+              searchToggle
+                ? "./img/ProfileClicked.svg"
+                : "./img/ProfileUnclicked.svg"
+            }
+            alt=""
+          />
+        </button>
+        <button onClick={() => setSearchToggle(false)}>
+          <img
+            src={
+              searchToggle
+                ? "./img/HashtagUnclicked.svg"
+                : "./img/HashtagClicked.svg"
+            }
+            alt=""
+          />
+        </button>
+        <div className="red_line"></div>
+      </div>
+      <section className="search_resuls"></section>
+    </main>
+  );
 };
 
 export default Search;
