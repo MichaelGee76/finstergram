986fd673b8f51ab616c2dedcb0d3183e2744c666
MichaelGee76 <gottungmichael@gmail.com>
2024-06-22 20:41:10 +0200

    API.JS TEST TEST TEST

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -8,66 +8,71 @@ import Post from "../../components/Post/Post";
 import Inbox from "../../components/Inbox/Inbox";
 
 const Home = () => {
-  const { token } = useContext(TokenDataContext);
-  const [feed, setFeed] = useState();
-  const [updUserFeed, setUpdUserFeed] = useState(false);
-  const [fixBG, setFixBg] = useState(false);
-  const [loadMorePosts, setLoadMorePosts] = useState(6);
+    const { token } = useContext(TokenDataContext);
+    const [feed, setFeed] = useState();
+    const [updUserFeed, setUpdUserFeed] = useState(false);
+    const [fixBG, setFixBg] = useState(false);
+    const [loadMorePosts, setLoadMorePosts] = useState(6);
 
-  useEffect(() => {
-    const getUserFeed = async () => {
-      const res = await ky
-        .get(`${backendUrl}/posts/userFeed`, {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        })
-        .json();
+    useEffect(() => {
+        const getUserFeed = async () => {
+            const res = await ky
+                .get(`${backendUrl}/posts/userFeed`, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    },
+                })
+                .json();
 
-      setFeed(res.result);
-    };
+            setFeed(res.result);
+        };
 
-    getUserFeed();
-  }, [updUserFeed]);
+        getUserFeed();
+    }, [updUserFeed]);
 
-  return (
-    <main className="dash_section" style={fixBG ? { overflow: "hidden" } : {}}>
-      <div className="dash_heading_div">
-        <div>
-          <img src="/img/LogoSmall.svg" alt="" />
-          <h1>TokTok</h1>
-        </div>
-        <div>
-          <Link to="/chatDashboard">
-            <img src="/img/Messanger.svg" alt="" />
-          </Link>
-          <Inbox />
-        </div>
-      </div>
+    return (
+        <main
+            className="dash_section"
+            style={fixBG ? { overflow: "hidden" } : {}}
+        >
+            <div className="dash_heading_div">
+                <div>
+                    <img src="/img/LogoSmall.svg" alt="" />
+                    <h1>TokTok</h1>
+                </div>
+                <div>
+                    <Link to="/chatDashboard">
+                        <img src="/img/Messanger.svg" alt="" />
+                    </Link>
+                    <Inbox />
+                </div>
+            </div>
 
-      <section className="posts_section">
-        {feed?.slice(0, loadMorePosts)?.map((post) => (
-          <Post
-            setFixBg={setFixBg}
-            key={post._id}
-            postData={post}
-            setUpdUserFeed={setUpdUserFeed}
-          />
-        ))}
-        {feed?.length >= loadMorePosts && (
-          <button
-            onClick={() =>
-              setLoadMorePosts((loadMorePosts) => loadMorePosts + 5)
-            }
-            className="more_posts_btn"
-          >
-            more posts
-          </button>
-        )}
-      </section>
-    </main>
-  );
+            <section className="posts_section">
+                {feed?.slice(0, loadMorePosts)?.map((post) => (
+                    <Post
+                        setFixBg={setFixBg}
+                        key={post._id}
+                        postData={post}
+                        setUpdUserFeed={setUpdUserFeed}
+                    />
+                ))}
+                {feed?.length >= loadMorePosts && (
+                    <button
+                        onClick={() =>
+                            setLoadMorePosts(
+                                (loadMorePosts) => loadMorePosts + 5
+                            )
+                        }
+                        className="more_posts_btn"
+                    >
+                        more posts
+                    </button>
+                )}
+            </section>
+        </main>
+    );
 };
 
 export default Home;
