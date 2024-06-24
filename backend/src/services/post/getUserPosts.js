153c86ac87b2c0f916e463048746e3a02490b034
MichaelGee76 <gottungmichael@gmail.com>
2024-06-24 13:52:51 +0200

    Fixed comments object

--- backend/src/services/post/getUserPosts.js
+++ backend/src/services/post/getUserPosts.js
@@ -48,7 +48,7 @@ export async function getUserPosts(userId, authenticatedUserId) {
             return {
                 ...post.toObject(),
                 likes,
-                comments,
+                comments: comments.length,
             };
         })
     );
