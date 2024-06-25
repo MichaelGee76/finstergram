beed8a4d79e06e29c4a80f9c0d38973b889ef4cc
MichaelGee76 <gottungmichael@gmail.com>
2024-06-25 19:55:34 +0200

    test code index.js not found issue 43!

--- backend/src/index.js
+++ backend/src/index.js
@@ -57,9 +57,7 @@
 // });
 
 // Fachkraft sagt die index muss eine catchall route haben.... lets try
-
 import express from "express";
-import path from "path";
 import morgan from "morgan";
 import cors from "cors";
 import { connect2DB } from "./utils/connect2DB.js";
@@ -73,45 +71,35 @@ import { google } from "googleapis";
 import dotenv from "dotenv";
 import { commentRouter } from "./routes/commentRouter.js";
 import cookieSession from "cookie-session";
-import httpProxy from "http-proxy";
-
-// Proxy-Server einrichten
-const proxy = httpProxy.createProxyServer({
-    target: "https://finstergram-1-1.onrender.com",
-});
 
 const PORT = process.env.PORT || 4420;
 
 connect2DB();
 
 const app = express();
-// Cors config start
+
+// Middleware für CORS, Logging und Cookies
 const twoWeeksInMs = 14 * 24 * 60 * 60 * 1000;
 const isFrontendLocalhost =
     process.env.FRONTEND_URL.startsWith("http://localhost");
 const cookieSessionSecret = process.env.COOKIE_SESSION_SECRET;
 
-// re-configure cors middleware
 app.use(cors({ origin: [process.env.FRONTEND_URL], credentials: true }));
-/////////// add parser for cookies
-app.set("trust proxy", 1); // trust first proxy
+app.set("trust proxy", 1);
 const cookieSessionOptions = {
     name: "session",
-    secret: cookieSessionSecret, // frei wählbar
+    secret: cookieSessionSecret,
     httpOnly: true,
     expires: new Date(Date.now() + twoWeeksInMs),
     sameSite: isFrontendLocalhost ? "lax" : "none",
     secure: isFrontendLocalhost ? false : true,
 };
 app.use(cookieSession(cookieSessionOptions));
-
-// cors config end
 app.use(morgan("dev"));
 app.use(express.json());
-//Wenn Express.js-Anwendung Formulardaten analysieren werden, die mit dem application/x-www-form-urlencoded MIME-Typ gesendet werden, dann ist diese Zeile notwendig. Andernfalls ist sie nicht erforderlich.
 app.use(express.urlencoded({ extended: false }));
 
-// Routes
+// Routen für API-Endpunkte
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/posts", postRouter);
 app.use("/api/v1/comments", commentRouter);
@@ -120,14 +108,12 @@ app.use("/api/v1/save", saveRouter);
 app.use("/api/v1/follow", followRouter);
 app.use("/api/v1/message", messageRouter);
 
-// Catch-all route to serve frontend for all other routes
-app.get("*", (req, res) => {
-    if (req.path.startsWith("/api/")) {
-        return res.status(404).json({ message: "API route not found" });
-    }
-    proxy.web(req, res);
+// Weiterleitung aller anderen Anfragen an
+app.use((req, res, next) => {
+    res.redirect(301, "https://www.mern-stack.de" + req.originalUrl);
 });
 
+// Server starten
 app.listen(PORT, () => {
     console.log("Server is listening on port: ", PORT);
 });
