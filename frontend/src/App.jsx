9867f1a83fd60f8f4f966fa80f75dbfa0dc796d0
MichaelGee76 <gottungmichael@gmail.com>
2024-06-26 09:09:53 +0200

    fixed index.html. Fixed email text. Try app.jsx code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,10 +9,10 @@ import Upload from "./pages/Upload/Upload";
 import Search from "./pages/Search/Search";
 import { useState, useEffect } from "react";
 import {
-  TokenDataContext,
-  UserDataContext,
-  ChatsDataContext,
-  ColorModeContext,
+    TokenDataContext,
+    UserDataContext,
+    ChatsDataContext,
+    ColorModeContext,
 } from "./components/context/Context";
 import Layout from "./components/Layout/Layout";
 import HashtagPosts from "./pages/HashtagPosts/HashtagPosts";
@@ -24,136 +24,147 @@ import SavedPosts from "./pages/SavedPosts/SavedPosts";
 import LikedPosts from "./pages/LikedPosts/LikedPosts";
 
 function App() {
-  const [user, setUser] = useState();
-  const [token, setToken] = useState();
-  const [isLoading, setIsLoading] = useState(true);
-  const [chats, setChats] = useState([]);
-  const [colorSelect, setColorSelect] = useState();
+    const [user, setUser] = useState();
+    const [token, setToken] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+    const [chats, setChats] = useState([]);
+    const [colorSelect, setColorSelect] = useState();
 
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 5000);
-  }, []);
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 5000);
+    }, []);
 
-  return (
-    <ColorModeContext.Provider value={{ colorSelect, setColorSelect }}>
-      <ChatsDataContext.Provider value={{ chats, setChats }}>
-        <UserDataContext.Provider value={{ user, setUser }}>
-          <TokenDataContext.Provider value={{ token, setToken }}>
-            {isLoading ? (
-              <Loading />
-            ) : (
-              <BrowserRouter>
-                <Layout>
-                  <Routes>
-                    <Route
-                      path="/"
-                      element={
-                        <AuthRequired>
-                          <Home />
-                        </AuthRequired>
-                      }
-                    />
-                    <Route
-                      path="/chat/:id"
-                      element={
-                        <AuthRequired>
-                          <Chat />
-                        </AuthRequired>
-                      }
-                    />
-                    <Route
-                      path="/chatDashboard"
-                      element={
-                        <AuthRequired>
-                          <ChatDashboard />
-                        </AuthRequired>
-                      }
-                    />
-                    <Route
-                      path="/newChat"
-                      element={
-                        <AuthRequired>
-                          <ChatDashboard />
-                        </AuthRequired>
-                      }
-                    />
-                    <Route
-                      path="/profile/:id"
-                      element={
-                        <AuthRequired>
-                          <Profile />
-                        </AuthRequired>
-                      }
-                    />
-                    <Route path="/signinup" element={<SignInUp />} />
-                    <Route
-                      path="/upload"
-                      element={
-                        <AuthRequired>
-                          <Upload />
-                        </AuthRequired>
-                      }
-                    />
-                    <Route
-                      path="/search"
-                      element={
-                        <AuthRequired>
-                          <Search />
-                        </AuthRequired>
-                      }
-                    />
-                    <Route
-                      path="/hashtagposts/:hashtag"
-                      element={
-                        <AuthRequired>
-                          <HashtagPosts />
-                        </AuthRequired>
-                      }
-                    />
+    return (
+        <ColorModeContext.Provider value={{ colorSelect, setColorSelect }}>
+            <ChatsDataContext.Provider value={{ chats, setChats }}>
+                <UserDataContext.Provider value={{ user, setUser }}>
+                    <TokenDataContext.Provider value={{ token, setToken }}>
+                        {isLoading ? (
+                            <Loading />
+                        ) : (
+                            <BrowserRouter>
+                                <Layout>
+                                    <Routes>
+                                        <Route
+                                            path="/"
+                                            element={
+                                                <AuthRequired>
+                                                    <Home />
+                                                </AuthRequired>
+                                            }
+                                        />
+                                        <Route
+                                            path="/chat/:id"
+                                            element={
+                                                <AuthRequired>
+                                                    <Chat />
+                                                </AuthRequired>
+                                            }
+                                        />
+                                        <Route
+                                            path="/chatDashboard"
+                                            element={
+                                                <AuthRequired>
+                                                    <ChatDashboard />
+                                                </AuthRequired>
+                                            }
+                                        />
+                                        <Route
+                                            path="/newChat"
+                                            element={
+                                                <AuthRequired>
+                                                    <ChatDashboard />
+                                                </AuthRequired>
+                                            }
+                                        />
+                                        <Route
+                                            path="/profile/:id"
+                                            element={
+                                                <AuthRequired>
+                                                    <Profile />
+                                                </AuthRequired>
+                                            }
+                                        />
+                                        <Route
+                                            path="/signinup"
+                                            element={<SignInUp />}
+                                        />
+                                        <Route
+                                            path="/upload"
+                                            element={
+                                                <AuthRequired>
+                                                    <Upload />
+                                                </AuthRequired>
+                                            }
+                                        />
+                                        <Route
+                                            path="/search"
+                                            element={
+                                                <AuthRequired>
+                                                    <Search />
+                                                </AuthRequired>
+                                            }
+                                        />
+                                        <Route
+                                            path="/hashtagposts/:hashtag"
+                                            element={
+                                                <AuthRequired>
+                                                    <HashtagPosts />
+                                                </AuthRequired>
+                                            }
+                                        />
 
-                    <Route
-                      path="/singlepost/:postId"
-                      element={
-                        <AuthRequired>
-                          <SinglePost />
-                        </AuthRequired>
-                      }
-                    />
-                    <Route
-                      path="/editpost/:id"
-                      element={
-                        <AuthRequired>
-                          <EditPost />
-                        </AuthRequired>
-                      }
-                    />
-                    <Route
-                      path="/savedposts"
-                      element={
-                        <AuthRequired>
-                          <SavedPosts />
-                        </AuthRequired>
-                      }
-                    />
-                    <Route
-                      path="/likedposts"
-                      element={
-                        <AuthRequired>
-                          <LikedPosts />
-                        </AuthRequired>
-                      }
-                    />
-                  </Routes>
-                </Layout>
-              </BrowserRouter>
-            )}
-          </TokenDataContext.Provider>
-        </UserDataContext.Provider>
-      </ChatsDataContext.Provider>
-    </ColorModeContext.Provider>
-  );
+                                        <Route
+                                            path="/singlepost/:postId"
+                                            element={
+                                                <AuthRequired>
+                                                    <SinglePost />
+                                                </AuthRequired>
+                                            }
+                                        />
+                                        <Route
+                                            path="/editpost/:id"
+                                            element={
+                                                <AuthRequired>
+                                                    <EditPost />
+                                                </AuthRequired>
+                                            }
+                                        />
+                                        <Route
+                                            path="/savedposts"
+                                            element={
+                                                <AuthRequired>
+                                                    <SavedPosts />
+                                                </AuthRequired>
+                                            }
+                                        />
+                                        <Route
+                                            path="/likedposts"
+                                            element={
+                                                <AuthRequired>
+                                                    <LikedPosts />
+                                                </AuthRequired>
+                                            }
+                                        />
+                                        <Route
+                                            path="/*"
+                                            element={
+                                                <AuthRequired>
+                                                    <Home />
+                                                </AuthRequired>
+                                            }
+                                        />
+                                    </Routes>
+                                </Layout>
+                            </BrowserRouter>
+                        )}
+                    </TokenDataContext.Provider>
+                </UserDataContext.Provider>
+            </ChatsDataContext.Provider>
+        </ColorModeContext.Provider>
+    );
 }
 
 export default App;
