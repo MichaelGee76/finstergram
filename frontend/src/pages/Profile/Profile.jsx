ed341ab0d81040fcc977ff6c517f252b0d20f422
Frederick Pawelzik <frederickpawelzik@gmail.com>
2024-06-26 11:26:58 +0200

    post-wrapper

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -378,14 +378,16 @@ const Profile = () => {
 
       {!isUser &&
         (!follow ? (
-          <button className="button_unclicked">
+          <button
+            onClick={() => postFollowing(id)}
+            className="button_unclicked"
+          >
             <svg
               width="21"
               height="21"
               viewBox="0 0 21 21"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              onClick={() => postFollowing(id)}
             >
               <path
                 fillRule="evenodd"
