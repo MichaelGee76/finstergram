0f362fb5fbeb35873cd92651101628852ec7d98c
Zied1981 <zkhemiri@gmail.com>
2024-06-26 10:29:44 +0200

    inbox gefixt

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,5 @@
-export const backendUrl =
-    import.meta.env.BACKEND_URL || "https://finstergram.onrender.com/api/v1";
+/* export const backendUrl = */
+/*     import.meta.env.BACKEND_URL || "https://finstergram.onrender.com/api/v1"; */
 
-// export const backendUrl =
-//     import.meta.env.BACKEND_URL || "http://localhost:4420/api/v1";
+export const backendUrl =
+  import.meta.env.BACKEND_URL || "http://localhost:4420/api/v1";
